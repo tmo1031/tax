cde38bced0eae70c5eb4613e456b406e6e67c241
tmarumo0831 <90425151+tmarumo0831@users.noreply.github.com>
2025-01-19 08:08:57 +0900

    Deploy to GitHub Pages

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,4 +1,15 @@
 "use strict";
-let message = 'Hello World';
-console.log(message);
+// ユーザーリストの作成
+const users = [
+    { id: 1, name: 'Alice', email: 'alice@example.com' },
+    { id: 2, name: 'Bob', email: 'bob@example.com' },
+];
+// ユーザーリストを表示する関数
+function displayUsers(users) {
+    users.forEach((user) => {
+        console.log(`ID: ${user.id}, Name: ${user.name}, Email: ${user.email}`);
+    });
+}
+// 関数の実行
+displayUsers(users);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
-let message: string = 'Hello World';
-console.log(message);
+// 型の定義
+type User = {
+  id: number;
+  name: string;
+  email: string;
+};
+
+// ユーザーリストの作成
+const users: User[] = [
+  { id: 1, name: 'Alice', email: 'alice@example.com' },
+  { id: 2, name: 'Bob', email: 'bob@example.com' },
+];
+
+// ユーザーリストを表示する関数
+function displayUsers(users: User[]): void {
+  users.forEach((user) => {
+    console.log(`ID: ${user.id}, Name: ${user.name}, Email: ${user.email}`);
+  });
+}
+
+// 関数の実行
+displayUsers(users);
